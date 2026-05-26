6406c2e5eb3fc7769db344e5b91b28ebc5e52d64
thiagocalmon <thiago.calmon4@gmail.com>
2026-05-26 18:14:03 -0300

    Update exercicios

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const inputCep = document.getElementById("cep")
+const btnCep = document.getElementById("btnCep")
+const resultadoCep = document.querySelector(".resultadoCep")
+
+btnCep.addEventListener("click", handleClick)
+
+function handleClick(event) {
+  event.preventDefault()
+  const cep = inputCep.value.trim()
+  searchCep(cep)
+}
+
+function searchCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => response.text())
+    .then((data) => {
+      resultadoCep.innerText = data
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar o CEP:", error)
+    })
+}
+
+const btcElement = document.querySelector(".btc")
+
+function fetchBtc() {
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
+    .then((data) => {
+      btcElement.innerText = `Preço do Bitcoin em USD: $${data.USD.last.toFixed(2)}`
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar o preço do Bitcoin:", error)
+    })
+}
+
+fetchBtc()
